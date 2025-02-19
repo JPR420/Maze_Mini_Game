0d9912b238ef76198c0d9e8a342d706fe4875c9f
HarisAkbar03 <ha.akbar03@gmail.com>
2025-02-19 12:38:51 -0500

    Fixed the robot movement and added some debugging tools

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,6 @@
 module org.example.mini_game {
     requires javafx.controls;
     requires javafx.fxml;
-<<<<<<< HEAD
-=======
-    requires javafx.graphics;
->>>>>>> da5a30d (Fixed the robot movement and added some debugging tools)
-
 
     opens org.example.mini_game to javafx.fxml;
     exports org.example.mini_game;
